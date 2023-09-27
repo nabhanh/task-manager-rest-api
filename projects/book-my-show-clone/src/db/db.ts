3bb4c2f800cbbf98a4993872aef5488c4aa83f5e
Nabhan Hanif <nabhanhanif2000@gmail.com>
2023-09-27 12:09:26 +0530

    fix: remove sensitive info

--- projects/book-my-show-clone/src/db/db.ts
+++ projects/book-my-show-clone/src/db/db.ts
@@ -3,9 +3,6 @@ import postgres from 'postgres';
 import 'dotenv';
 import * as schema from './schema';
 
-const sql = postgres(
-  `postgresql://postgres:QaZlJBsljHNG57WH@db.sjwzstsdjljsutokwbio.supabase.co:5432/postgres`,
-  { max: 1 }
-);
+const sql = postgres(process.env.DB_URL as string, { max: 1 });
 const db = drizzle(sql, { schema });
 export default db;
